cec7e86918f6247654c46e5951b7af8af96ff8ee
berkercanatar <berkercanatar@gmail.com>
2020-12-11 02:51:01 +0300

    Update drone control features

---
+++ src/com/javigation/DroneConnection.java
@@ -0,0 +1,73 @@
+package com.javigation;
+
+import io.mavsdk.System;
+import java.util.ArrayList;
+
+public class DroneConnection implements MavSDKServerReadyListener {
+
+    public static ArrayList<DroneConnection> Connections = new ArrayList<DroneConnection>();
+
+    public MavSDKServer server;
+    public System drone;
+    public int MavlinkPort;
+    public int MavSDKPort;
+    public int VideoPort;
+
+    private static final int MAVLINK_BASE_PORT = 14540;
+    private static final int MAVSDK_SERVER_BASE_PORT = 4790;
+    private static final int VIDEO_BASE_PORT = 5600;
+
+    private boolean isDroneConnected = false;
+
+    private DroneConnection(int incomingMavlinkPort) {
+        this(incomingMavlinkPort, MAVSDK_SERVER_BASE_PORT + (incomingMavlinkPort - MAVLINK_BASE_PORT), VIDEO_BASE_PORT + (incomingMavlinkPort - MAVLINK_BASE_PORT));
+    }
+
+    private DroneConnection(int incomingMavlinkPort, int localMavSDKPort, int videoPort) {
+        MavlinkPort = incomingMavlinkPort;
+        MavSDKPort = localMavSDKPort;
+        VideoPort = videoPort;
+        server = new MavSDKServer(this, incomingMavlinkPort, localMavSDKPort);
+    }
+
+    public static DroneConnection Get(int mavlinkPort) {
+        DroneConnection connection = Utils.findOneBy(Connections, con -> con.MavlinkPort == mavlinkPort);
+        if ( connection == null )
+            connection = new DroneConnection(mavlinkPort);
+            Connections.add(connection);
+        return connection;
+    }
+    public static DroneConnection Get(DroneConnection droneConnection) { //Ensures given DroneConnection is in the static list.
+        DroneConnection connection = Utils.findOneBy(Connections, con -> con.MavlinkPort == droneConnection.MavlinkPort);
+        if ( connection == null )
+            Connections.add(droneConnection);
+        return connection;
+    }
+
+    public static DroneConnection GetByVideoPort(int videoPort) {
+        return Utils.findOneBy(Connections, con -> con.VideoPort == videoPort);
+    }
+    @Override
+    public void onServerInitialized() {
+        drone = new System("127.0.0.1", MavSDKPort);
+        DroneConnection connection = this;
+    }
+
+    public static void onDroneConnected(DroneConnection connection) {
+        if (connection.isDroneConnected)
+            return;
+        connection.isDroneConnected = true;
+
+        java.lang.System.out.println("CONNECTED " + connection.MavlinkPort);
+    }
+
+    public static void onDroneDisconnected(DroneConnection connection) {
+        if (!connection.isDroneConnected)
+            return;
+        connection.isDroneConnected = false;
+
+        java.lang.System.out.println("DISCONNECTED " + connection.MavlinkPort);
+    }
+
+
+}
---
+++ src/com/javigation/MavSDKServer.java
@@ -0,0 +1,72 @@
+package com.javigation;
+
+import com.sun.jna.Platform;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+interface MavSDKServerReadyListener {
+    void onServerInitialized();
+}
+
+public class MavSDKServer  {
+
+    DroneConnection connection;
+
+    public MavSDKServer(DroneConnection connection, int incomingMavlinkPort, int localMavSDKPort) {
+        String mavSdkProcPath = "";
+        if (Platform.isLinux()) {
+            mavSdkProcPath = MavSDKServer.class.getClassLoader().getResource("mavsdk_server/mavsdk_server_manylinux2010-x64").getPath();
+        } else if (Platform.isWindows()) {
+            mavSdkProcPath = MavSDKServer.class.getClassLoader().getResource("mavsdk_server/mavsdk_server_win32.exe").getPath();
+        }
+
+        this.connection = connection;
+
+        launch(mavSdkProcPath, "-p", Integer.toString(localMavSDKPort),  "udp://:" + incomingMavlinkPort);
+
+    }
+
+    private void launch(final String ... args) {
+        String threadID = "MAVSDK-Server";
+
+        new Thread(new Runnable() {
+            public void run() {
+                ProcessBuilder b = new ProcessBuilder(args);
+                b.redirectErrorStream(true);
+                try {
+                    Process process = b.start();
+
+                    Thread shutdownMavlinkThread = new Thread(() -> process.destroy());
+                    Runtime.getRuntime().addShutdownHook(shutdownMavlinkThread);
+
+                    readOutput(process);
+                    process.waitFor();
+                    process.destroy();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            private void readOutput(Process process) throws IOException {
+                BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                String line;
+                while ((line = in.readLine()) != null) {
+                    System.err.println(line);
+                    if (line.contains("Server set to listen"))
+                        connection.onServerInitialized();
+                    else if (line.contains("Discovered"))
+                        DroneConnection.onDroneConnected(connection);
+                    else if (line.contains("Lost"))
+                        DroneConnection.onDroneDisconnected(connection);
+                }
+            }
+        }, threadID).start();
+    }
+
+
+}
