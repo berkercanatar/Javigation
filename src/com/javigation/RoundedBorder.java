2ea39c5b0ae236628a81587a3683fa8a42e9f8b3
berkercanatar <berkercanatar@gmail.com>
2020-12-11 20:48:09 +0300

    Update RoundedBorder to draw custom background

--- src/com/javigation/RoundedBorder.java
+++ src/com/javigation/RoundedBorder.java
@@ -1,6 +1,6 @@
 package com.javigation;
 
-//CODE FROM : https://stackoverflow.com/questions/15025092/border-with-rounded-corners-transparency
+//INSPIRED FROM : https://stackoverflow.com/questions/15025092/border-with-rounded-corners-transparency
 
 import java.awt.*;
 import java.awt.geom.Area;
@@ -17,6 +17,9 @@ class RoundedBorder extends AbstractBorder {
     private int strokePad;
     RenderingHints hints;
 
+    boolean customBg = false;
+    Color colorBg;
+
 
     RoundedBorder(
             Color color, int thickness, int radii) {
@@ -28,12 +31,17 @@ class RoundedBorder extends AbstractBorder {
         strokePad = thickness / 2;
 
         hints = new RenderingHints(
-                RenderingHints.KEY_ANTIALIASING,
-                RenderingHints.VALUE_ANTIALIAS_ON);
+                RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
         int pad = radii + strokePad;
         int bottomPad = pad + strokePad;
         insets = new Insets(pad, pad, bottomPad, pad);
+    }
+
+    RoundedBorder(Color color, int thickness, int radii, Color colorBg) {
+        this(color, thickness, radii);
+        this.colorBg = colorBg;
+        this.customBg = true;
     }
 
     @Override
@@ -57,7 +65,7 @@ class RoundedBorder extends AbstractBorder {
 
         int bottomLineY = height - thickness;
 
-        RoundRectangle2D.Double bubble = new RoundRectangle2D.Double(
+        RoundRectangle2D.Double roundRectangle = new RoundRectangle2D.Double(
                 0 + strokePad,
                 0 + strokePad,
                 width - thickness,
@@ -65,17 +73,27 @@ class RoundedBorder extends AbstractBorder {
                 radii,
                 radii);
 
-        Area area = new Area(bubble);
+        Area area = new Area(roundRectangle);
 
         g2.setRenderingHints(hints);
+        g2.setRenderingHint(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE);
+        g2.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
 
         Component parent  = c.getParent();
         if (parent!=null) {
             Color bg = parent.getBackground();
+            if (customBg)
+                bg = colorBg;
             Rectangle rect = new Rectangle(0,0,width, height);
             Area borderRegion = new Area(rect);
             borderRegion.subtract(area);
-            g2.setClip(borderRegion);
+            Area borderRegion2 = new Area(rect);
+            borderRegion2.subtract(borderRegion);
+            if (customBg)
+                g2.setClip(borderRegion2);
+            else
+                g2.setClip(borderRegion);
             g2.setColor(bg);
             g2.fillRect(0, 0, width, height);
             g2.setClip(null);
