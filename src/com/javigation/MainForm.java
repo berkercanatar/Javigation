6d24830ba4352cf4ec6f798c8173b6a5ae2c35fe
berkercanatar <berkercanatar@gmail.com>
2020-12-11 20:51:38 +0300

    Remove unnecessary imports

--- src/com/javigation/MainForm.java
+++ src/com/javigation/MainForm.java
@@ -1,15 +1,5 @@
 package com.javigation;
 
-import io.mavsdk.action.Action;
-import io.mavsdk.mission.Mission;
-import io.mavsdk.offboard.Offboard;
-import org.jxmapviewer.*;
-import org.jxmapviewer.input.MapClickListener;
-import org.jxmapviewer.input.PanMouseInputListener;
-import org.jxmapviewer.input.ZoomMouseWheelListenerCursor;
-import org.jxmapviewer.painter.CompoundPainter;
-import org.jxmapviewer.viewer.*;
-
 import javax.swing.*;
 
 public class MainForm extends JFrame {
