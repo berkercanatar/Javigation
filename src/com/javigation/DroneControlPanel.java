da7b83e29c8522b24c735a2e57ef7d143a09d340
berkercanatar <berkercanatar@gmail.com>
2020-12-11 20:48:44 +0300

    Add custom panel shape

--- src/com/javigation/DroneControlPanel.java
+++ src/com/javigation/DroneControlPanel.java
@@ -1,9 +1,16 @@
 package com.javigation;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 
 public class DroneControlPanel extends JPanel {
 
-
+    public DroneControlPanel() {
+        setPreferredSize(new Dimension(50,200));
+        setBackground(GUIManager.COLOR_TRANSPARENT);
+        setBorder(new RoundedBorder(Color.BLACK, 5, 20, new Color(GUIManager.COLOR_PURPLE.getRed(), GUIManager.COLOR_PURPLE.getGreen(), GUIManager.COLOR_PURPLE.getBlue(), 200)));
+        add(new JButton("test"));
+    }
 
 }
