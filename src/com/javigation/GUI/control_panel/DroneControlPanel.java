da9d283162ff1942b44d57975ccefc85d215e0b2
bagcifurkan <furkan.bagci@ug.bilkent.edu.tr>
2020-12-17 04:20:10 +0300

    Update DroneControlPanel.java

--- src/com/javigation/GUI/control_panel/DroneControlPanel.java
+++ src/com/javigation/GUI/control_panel/DroneControlPanel.java
@@ -2,17 +2,29 @@ package com.javigation.GUI.control_panel;
 
 import com.javigation.GUI.GUIManager;
 import com.javigation.GUI.RoundedBorder;
+import com.javigation.Utils;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Locale;
 
 public class DroneControlPanel extends JPanel {
 
     public DroneControlPanel() {
-        setPreferredSize(new Dimension(50,200));
+        setPreferredSize(new Dimension(400,400));
+        setLayout(new GridLayout(4,2));
         setBackground(GUIManager.COLOR_TRANSPARENT);
-        setBorder(new RoundedBorder(Color.BLACK, 5, 20, new Color(GUIManager.COLOR_PURPLE.getRed(), GUIManager.COLOR_PURPLE.getGreen(), GUIManager.COLOR_PURPLE.getBlue(), 200)));
-        add(new JButton("test"));
+        setBorder(new RoundedBorder(Color.BLACK, 5, 20, Utils.colorWithAlpha(GUIManager.COLOR_PURPLE, 0.25f)));
+
+        generateButtons();
+
+    }
+
+    private void generateButtons() {
+        for ( DroneControlPanelButton.ButtonType buttonType : DroneControlPanelButton.ButtonType.values() ) {
+           add(new DroneControlPanelButton(this, buttonType));
+        }
+
     }
 
 }
