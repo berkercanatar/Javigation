33906792a0ae4df43bc0bc145c501727e026e7c9
bagcifurkan <furkan.bagci@ug.bilkent.edu.tr>
2020-12-17 04:18:34 +0300

    Add Map function

--- src/com/javigation/Utils.java
+++ src/com/javigation/Utils.java
@@ -3,6 +3,7 @@ package com.javigation;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.function.Predicate;
@@ -22,6 +23,15 @@ public final class Utils {
 
     public static void info(Object o) {
         logger.info(o);
+    }
+
+    public static Color colorWithAlpha(Color color, float opacity) {
+        System.out.println(Map(opacity, 0f,1f, 0f,255f));
+        return new Color(color.getRed(), color.getGreen(), color.getBlue(), (int) Map(opacity, 0f,1f, 255f,0f));
+    }
+
+    public static float Map( float value, float sourceFrom, float sourceTo, float targetFrom, float targetTo) {
+        return ((value - sourceFrom) / (sourceTo - sourceFrom) * (targetTo - targetFrom) + targetFrom);
     }
     
 }
