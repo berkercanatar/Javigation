37d2bc65690ed74ea89b3d43f3959da31cdbfe8d
Berker Canatar <berker.canatar@ug.bilkent.edu.tr>
2020-12-17 20:35:19 +0300

    Create Statics.java

---
+++ src/com/javigation/Statics.java
@@ -0,0 +1,15 @@
+package com.javigation;
+
+import java.nio.file.Path;
+
+public final class Statics {
+
+    public static String JAVIGATION_DATA_FOLDER;
+    public static Path JAVIGATION_FOLDER;
+
+    public static final String GSTREAMER_x64_URL = "https://gstreamer.freedesktop.org/data/pkg/windows/1.18.2/mingw/gstreamer-1.0-mingw-x86_64-1.18.2.msi";
+    public static final String GSTREAMER_x86_URL = "https://gstreamer.freedesktop.org/data/pkg/windows/1.18.2/mingw/gstreamer-1.0-mingw-x86-1.18.2.msi";
+
+    public static Path GSTREAMER_ROOT;
+
+}
