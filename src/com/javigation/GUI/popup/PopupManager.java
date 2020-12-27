2ffd0db43edcf7666c50d9a7575bd74630099810
Turgut <alp.edis@ug.bilkent.edu.tr>
2020-12-28 02:46:35 +0300

    Update Popup

--- src/com/javigation/GUI/popup/PopupManager.java
+++ src/com/javigation/GUI/popup/PopupManager.java
@@ -9,22 +9,24 @@ import java.awt.event.ActionListener;
 
 public class PopupManager {
 
-    JWindow popup;
+
     private float alpha = 0f;
     private long start = -1;
     private long runtime = 2000;
 
     private static JLabel messageLabel;
+    private static JPanel panel;
 
     public static void setup() {
         //CREATE POPUP PANEL or COMPONENT
-
+        panel = new JPanel();
         messageLabel = new JLabel();
-        //messageLabel.setFont();
-
-        Containers.popupContainer.add(messageLabel);
-
-        Containers.popupContainer.add(new JButton());
+        messageLabel.setVerticalAlignment(SwingConstants.TOP);
+        panel.setLayout( new BorderLayout() );
+        panel.setPreferredSize(new Dimension(800, 100));
+        panel.add( messageLabel, BorderLayout.WEST );
+        Containers.popupContainer.setBackground( panel.getBackground() );
+        Containers.popupContainer.add(panel);
         Containers.popupContainer.setVisible(false);
     }
 
@@ -35,62 +37,53 @@ public class PopupManager {
         SUCCESS
     }
 
-
-    public PopupManager(String message, PopupType type){
-
-        popup = new JWindow();
-        Color color = getType(type);
-        Color colorText = getContrastColor( color );
-
-        popup.setBackground(color);
-
-
-        JPanel pnl = new JPanel(){
-            @Override
-            protected void paintComponent(Graphics g) {
-
-                g.setColor( colorText );
-                g.drawString( message, 30, 25);
-
-            }
-        };
-        pnl.setPreferredSize( new Dimension(1000, 50));
-        popup.add( pnl );
-        popup.setLocation(450, 100);
-        popup.pack();
-
-        showIt();
-
-
-    }
-
     public static void showAlert(String message){
-        //new PopupManager( message, PopupType.WARNING);
 
-        //SET BACKGROUNDS
+        panel.setBackground( getColor( PopupType.WARNING ) );
+        messageLabel.setForeground( getContrastColor( panel.getBackground() ));
+        messageLabel.setFont(new Font("TimesRoman", Font.ITALIC, 20));
+        Containers.popupContainer.setBackground( panel.getBackground() );
         messageLabel.setText(message);
-        Containers.popupContainer.setVisible(true);
         //START TIMER
+        showIt();
     }
 
     public static void showError(String message){
-        new PopupManager( message, PopupType.ERROR);
+        panel.setBackground( getColor( PopupType.ERROR ) );
+        messageLabel.setForeground( getContrastColor( panel.getBackground() ));
+        messageLabel.setFont( new Font("TimesRoman", Font.BOLD, 20) );
+        Containers.popupContainer.setBackground( panel.getBackground() );
+        messageLabel.setText(message);
+        //START TIMER
+        showIt();
     }
 
     public static void showInfo(String message){
-        new PopupManager( message, PopupType.INFO);
+        panel.setBackground( getColor( PopupType.INFO ) );
+        messageLabel.setForeground( getContrastColor( panel.getBackground() ));
+        messageLabel.setFont(new Font("TimesRoman", Font.PLAIN, 20));
+        Containers.popupContainer.setBackground( panel.getBackground() );
+        messageLabel.setText(message);
+        //START TIMER
+        showIt();
     }
 
-    public static void showComplete(String message){
-        new PopupManager( message, PopupType.SUCCESS);
+    public static void showSuccess(String message){
+        panel.setBackground( getColor( PopupType.SUCCESS ) );
+        messageLabel.setForeground( getContrastColor( panel.getBackground() ));
+        Containers.popupContainer.setBackground( panel.getBackground() );
+        messageLabel.setFont(new Font("TimesRoman", Font.PLAIN, 20));
+        messageLabel.setText(message);
+        //START TIMER
+        showIt();
     }
 
-    private void showIt() {
-        popup.setVisible(true);
+    private static void showIt() {
+        Containers.popupContainer.setVisible(true);
 
         Timer t = new Timer(1000, e -> {
             if ( ((Timer) e.getSource()).getDelay() == 1000) {
-                popup.setVisible(false);
+                Containers.popupContainer.setVisible(false);
                 ((Timer) e.getSource()).stop();
             }
         });
@@ -98,12 +91,12 @@ public class PopupManager {
         t.start();
     }
 
-    private Color getContrastColor(Color color) {
+    private static Color getContrastColor(Color color) {
         double y = (299 * color.getRed() + 587 * color.getGreen() + 114 * color.getBlue()) / 1000;
         return y >= 128 ? Color.black : Color.white;
     }
 
-    private Color getType(PopupType type) {
+    private static Color getColor(PopupType type) {
 
         Color color = switch (type) {
             case ERROR -> Color.red;
